7e278236b5c7eed7ee55485745b28793147a8870
Achel Cano <hazelbre7@gmail.com>
2021-06-11 18:26:31 -0500

    Practica de python con Git

---
+++ Pryecto.py
@@ -0,0 +1,10 @@
+def main ():
+    print ("Probando el uso de git para ser un mejor programador a futuro")
+    palabra = input("Por favor escribe una palabra  ")
+    for i in range (10):
+        print(palabra)
+
+
+
+if __name__ == '__main__':
+    main()
